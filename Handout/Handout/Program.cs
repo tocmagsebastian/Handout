d9095590b33649c25358a2c9a990340b1ad6499c
tocmagsebastian <contsteam13@gmail.com>
2024-04-09 20:56:00 +0300

    Handout GetMin

---
+++ Handout/Handout/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter two integers:");
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+
+        int min = GetMin(num1, num2);
+
+        Console.WriteLine("The smallest of the two integers is: " + min);
+    }
+
+    static int GetMin(int a, int b)
+    {
+        return Math.Min(a, b);
+    }
+}
